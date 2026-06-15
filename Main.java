20e13ad410a7bfdcff7df4a27016c606e653a86b
Miller <miltaloza@gmail.com>
2026-06-15 17:07:40 +0200

    Camio en Rama Version

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class GitA {
 
 	public static void main(String[] args) {
 		System.out.println("Hola tod@s");
-		System.out.println("Hola Mundo");
+		System.out.println("Cambio en Rama1");
 	}
 
 }
