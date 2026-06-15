d40e10b32639ad5f0b53df855763ff1e771a3e47
Millersss22 <jmtalozaserador2003@gmail.com>
2026-06-15 17:16:02 +0200

    Commit en Github

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class GitA {
 		System.out.println("Hola tod@s");
 		System.out.println("Cambio en Principal");
 		System.out.println("Cambio en Rama1");
+		System.out.println("Cambio en Github");
 
 	}
 
